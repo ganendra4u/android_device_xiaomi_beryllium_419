5ddf241d97e11f9c705dde452cccb2433308050c
Ｇａｎｘ <daffaofficial224@gmail.com>
2026-07-08 10:20:27 +0700

    Add files via upload

---
+++ axion_beryllium.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, device/xiaomi/beryllium/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+PRODUCT_NAME := axion_beryllium
+PRODUCT_DEVICE := beryllium
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := POCO F1
+PRODUCT_MANUFACTURER := Xiaomi
